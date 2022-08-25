3ecdba92622bc1d7e29f09c92ac46a5a101fbd98
Andrey <asvidovskiy@gmail.com>
2022-08-25 21:06:34 +0300

    method animation downUp done

--- src/js/library/modules/animation.js
+++ src/js/library/modules/animation.js
@@ -1,6 +1,6 @@
 import $ from '../core';
 
-$.prototype.animation = function(e, duration, callback) {
+$.prototype.animation = function(e = 0, duration, callback) {
   let timeStart;
   let timePrev = 0;
   let end = false;
@@ -44,7 +44,6 @@ $.prototype.fadeIn = function({e = 0, duration, toNumber = 1, typeCount = 'opaci
 };
 
 $.prototype.fadeOut = function({e = 0, duration, toNumber = 1}) {
-  console.log(e.target)
   
   for (let i = 0; i < this.length; i++) {
     const fadeInAnimation = (elapsed, end) => {
@@ -70,17 +69,59 @@ $.prototype.fadeToggle = function({duration}) {
   }
 };
 
-$.prototype.downUp = function({e = 0, duration, toNumber = 100, typeCount = 'height'}) {
+$.prototype.downUp = function({e = 0, duration, typeCount = 'height'}) {
   for (let i = 0; i < this.length; i++) {
-    const fadeInAnimation = (elapsed, end) => {
-      const count = Math.min(elapsed / duration, toNumber);
-      this.selector[i].style.cssText = 'display';
-      this.selector[i].style.cssText = `${typeCount}: ${count};`;
-      console.log(elapsed, 'elapsed')
-        if (count >= toNumber) {
-          end = true;
+    this.selector[i].addEventListener('click', (e) => {
+      const active = this.selector[i].parentNode.classList.contains('active-content');
+      if (!active) {
+        let countHeight = 0;
+        let countPaddingTop = 0;
+        let countPaddingBottom = 0;
+        this.selector[i].parentNode.classList.add('active-content');
+        const paddingTop = +window.getComputedStyle(this.selector[i].nextElementSibling).paddingTop.replace(/px/, '');
+        const paddingBottom = +window.getComputedStyle(this.selector[i].nextElementSibling).paddingBottom.replace(/px/, '');
+        const height = +window.getComputedStyle(this.selector[i].nextElementSibling).height.replace(/px/, '');
+
+        const animate = (elapsed) => {
+          countHeight = Math.min(elapsed * (height / duration), height);
+          countPaddingTop = Math.min(elapsed * (paddingTop / (duration / 2)), paddingTop);
+          countPaddingBottom = Math.min(elapsed * (paddingBottom / (duration / 2)), paddingBottom);
+          this.selector[i].nextElementSibling.style.cssText = 'display';
+          this.selector[i].nextElementSibling.style.cssText = `
+          ${typeCount}: ${countHeight}px; 
+          padding-top: ${countPaddingTop}px; 
+          padding-bottom: ${countPaddingBottom}px;
+          `;
         }
-    }
-    $.prototype.animation(e, duration, fadeInAnimation)
+
+        $.prototype.animation(e, duration, animate);
+      } else {
+        let countHeight = 0;
+        let countPaddingTop = 0;
+        let countPaddingBottom = 0;
+        
+        const paddingTop = +window.getComputedStyle(this.selector[i].nextElementSibling).paddingTop.replace(/px/, '');
+        const paddingBottom = +window.getComputedStyle(this.selector[i].nextElementSibling).paddingBottom.replace(/px/, '');
+        const height = +window.getComputedStyle(this.selector[i].nextElementSibling).height.replace(/px/, '');
+        const animate = (elapsed) => {
+          countHeight = Math.min(elapsed * (height / duration), height);
+          countPaddingTop = Math.min(elapsed * (paddingTop / duration), paddingTop);
+          countPaddingBottom = Math.min(elapsed * (paddingBottom / duration), paddingBottom);
+          
+          this.selector[i].nextElementSibling.style.cssText = `
+          ${typeCount}: ${height - countHeight}px; 
+          padding-top: ${paddingTop - countPaddingTop}px; 
+          padding-bottom: ${paddingBottom - countPaddingBottom}px;
+          `;
+          if ((height - countHeight) <= 0) {
+            this.selector[i].parentNode.classList.remove('active-content');
+            this.selector[i].nextElementSibling.style = '';
+          }
+        }
+
+        $.prototype.animation(e, duration, animate);
+      }
+      
+    });
   }
 };
